f118bd93651e8af163b5f8c9a816fcb527df240e
hajar <hajar.haouat@student.vinci.be>
2023-10-09 14:35:04 +0200

    2.1 : HMI : first external script

--- exercises/2.1/external-script.js
+++ exercises/2.1/external-script.js
@@ -1,1 +1,8 @@
-alert("Pop-up thanks to an external JS file.");
+function addDateTime(message){
+    const dateTimeNow = new Date();
+    const date= dateTimeNow.toLocaleDateString() + dateTimeNow.toLocaleTimeString();
+    console.log(date);
+    return date +" : "+message;
+}
+
+alert(addDateTime("hello"));
